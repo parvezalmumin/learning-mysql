b91e9e06bf018f1a6835547d7f9d9285a0c5c6eb
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-03-18 16:12:31 -0700

    added sql problem 01

---
+++ Problem_01.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE xyz_company;
+USE xyz_company;
+
+CREATE TABLE employee (
+id INT PRIMARY KEY,
+name VARCHAR(100),
+salary INT
+);
+
+INSERT INTO employee
+(id, name, salary)
+VALUES
+(1,"Adam", 25000),
+(2,"Bob", 30000),
+(3,"Casey", 40000);
+
+SELECT * FROM employee;
+
+
+
+
+
